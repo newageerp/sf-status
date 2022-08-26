4152990d12d6dba7ffb52840eb0f3d1e4807d165
Arturas Jegorenkovas <jegorenkov@gmail.com>
2022-08-26 10:45:26 +0000

    statusDisableScopeAll

--- Controller/StatusController.php
+++ Controller/StatusController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Newageerp\SfBaseEntity\Controller\OaBaseController;
 use Newageerp\SfControlpanel\Console\LocalConfigUtils;
+use Newageerp\SfAuth\Service\AuthService;
 
 /**
  * @Route(path="/app/nae-core/status")
